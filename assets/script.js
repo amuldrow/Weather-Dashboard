d0fd49f9536eb9539238501a127f983e9e6ffd4a
A DIONN <ashley.muldrow@yahoo.com>
2021-12-12 12:01:03 -0500

    buttons js

--- assets/script.js
+++ assets/script.js
@@ -19,3 +19,33 @@ $('.search').on("click", function (event) {
     getHistory();
     getWeatherToday();
 });
+
+// will create buttons for recent cities searched 
+var conthistoryEl = $('.cityhistory');
+function getHistory(){
+    conthistoryEl.empty();
+
+    for (let i = 0; i < cityhistory.length; i++) {
+        var rowEl = $('<row>');
+        var btnEl = $('<button>').text('${cityhistory[i]}')
+
+        rowEl.addClass('row histBtnRow');
+		btnEl.addClass('btn btn-outline-secondary histBtn');
+		btnEl.attr('type', 'button');
+
+        contHistEl.prepend(rowEl);
+		rowEl.append(btnEl);
+	} if (!city) {
+		return;
+    }
+    // buttons start search 
+    $('.histBtn').on("click", function (event) {
+		event.preventDefault();
+		city = $(this).text();
+		fiveForecastEl.empty();
+		getWeatherToday();
+	});
+
+    // main Today card body 
+    var cardTodayBody = $('.cardBodyToday')
+    // applies weather info to weather today card and 5 day forecast
