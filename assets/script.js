7c9445c9edbd13438d3d85160e8d45ce7d7f0fe4
A DIONN <ashley.muldrow@yahoo.com>
2021-12-12 11:05:40 -0500

    js date/time/key

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,8 @@
-var user
+var key = '6e24ae4caaceaa4df66eb50fd45e65aa'
+var city = ''
+
+// get current date and time 
+var date = moment().format('dddd, MMMM Do YYYY');
+var time = moment().format('YYYY-MM-DD HH:MM:SS');
+
+var cityhistory = [];
