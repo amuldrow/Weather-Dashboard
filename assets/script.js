5c0cd4181278cb64ce05c4c5733d1e39c2877cd2
A DIONN <ashley.muldrow@yahoo.com>
2021-12-12 12:59:19 -0500

    js updates

--- assets/script.js
+++ assets/script.js
@@ -49,3 +49,61 @@ function getHistory(){
     // main Today card body 
     var cardTodayBody = $('.cardBodyToday')
     // applies weather info to weather today card and 5 day forecast
+    function getWeatherToday() {
+        var getURLCurrent = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}';
+
+        $(cardTodayBody).empty();
+
+        $.ajax({
+            url: getURLCurrent,
+            method: 'GET',
+
+        }).then(function (response) {
+            $('.cardTodayCityName').text(response.name);
+            $('.cardTodayDate').text(date);
+            //icons
+            $('.icons').attr('src', 'https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png');
+            //Temp
+            var pEl = $('<p>').text('Temperature: ${response.main.temp}°F');
+            cardTodayBody.append(pEl);
+            // Feels like
+            var pElTemp = $('<p>').text('Feels Like: ${response.main.feels_like}°F');
+            cardTodayBody.append(pElTemp);
+            //Humidity
+            var pElHumid = $('<p>').text('Humidity: ${response.humidity}°F');
+            cardTodayBody.append(pElHumid);
+            //Wind Speed
+            var pElWind = $('<p>').text('Wind speed: ${response.wind.speed}MPH');
+            cardTodayBody.append(pElWind);
+            //Set Lattitude and longitude for city 
+            var CityLon = response.coord.lon;
+            var Citylat = response.coord.lat;
+
+            var getURLUvi = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&exclude=hourly,daily,minutely&appid=${key}`;
+
+            $.ajax({
+                url: getURLUvi,
+                method: 'GET'
+            }).then(function (response) {
+                var pElUvi = $('<p>').text(`UV Index: `);
+			var uviSpan = $('<span>').text(response.current.uvi);
+			var uvi = response.current.uvi;
+			pElUvi.append(uviSpan);
+			cardTodayBody.append(pElUvi);
+			//set the UV index to match an exposure chart severity based on color 
+			if (uvi >= 0 && uvi <= 2) {
+				uviSpan.attr('class', 'green');
+			} else if (uvi > 2 && uvi <= 5) {
+				uviSpan.attr("class", "yellow")
+			} else if (uvi > 5 && uvi <= 7) {
+				uviSpan.attr("class", "orange")
+			} else if (uvi > 7 && uvi <= 10) {
+				uviSpan.attr("class", "red")
+			} else {
+				uviSpan.attr("class", "purple")
+			}
+		});
+            }
+            })
+        }
+    }
