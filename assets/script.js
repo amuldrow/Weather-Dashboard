63d14cd9a1a868ca36e82d62723686bcfa9c5076
A DIONN <ashley.muldrow@yahoo.com>
2021-12-12 11:39:33 -0500

    city history js

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,16 @@ var date = moment().format('dddd, MMMM Do YYYY');
 var time = moment().format('YYYY-MM-DD HH:MM:SS');
 
 var cityhistory = [];
+$('.search').on("click", function (event) {
+    event.preventDefault();
+    city = $(this).parent('.btnPar').siblings('.textVal').val().trim();
+    if (city === "") {
+        return;
+    };
+    cityhistory.push(city);
+
+    localStorage.setItem('city', JSON.stringify(cityhistory));
+    fivedayForecastEl.empty();
+    getHistory();
+    getWeatherToday();
+});
